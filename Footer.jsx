7dcb767712660e3537a0271ec2f4a3a33a5036b3
Anu Jose <56553091+anujos@users.noreply.github.com>
2021-06-05 20:56:11 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+
+  var curryear = new Date().getFullYear();
+  return (
+    <footer>
+      <p> Copyright @2021 </p>
+      </footer>
+  );
+
+}
+export default Footer;
